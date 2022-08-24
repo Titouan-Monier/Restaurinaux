977f6d2995f9e561a999c6f0dd7accc1f144190b
TitouanMonier <titouanmonier.pro@gmail.com>
2022-08-24 22:14:14 +0200

    ajout barre de recherche dans le crud

--- crudfile.php
+++ crudfile.php
@@ -7,21 +7,30 @@ $isAdmin = $isConnected && ($_SESSION['admin'] === "1");
 // redirection si l'user n'est pas admin
 if(!$isConnected || !$isAdmin){
     header("Location: login.php");
-    exit(); 
+    exit();
 
   }
 $pdo= new PDO ('mysql:host=localhost; port=3306; dbname=restaurinaux', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$search = $_GET['search'] ?? '';
+if ($search) {
+  $statement = $pdo->prepare('SELECT * FROM restaurant WHERE nom LIKE :nom ORDER BY id DESC');
+  $statement->bindValue(':nom', "%$search%");
+}
+else {
+  $statement = $pdo->prepare('SELECT * FROM restaurant ORDER BY id DESC');
+}
 
-$statement = $pdo->prepare('SELECT * FROM restaurant ORDER BY id DESC');
 $statement->execute();
 $restaurants = $statement->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 
 <!doctype html>
 <html lang="en">
   <head>
+    <link rel="stylesheet" href="app.css" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Restaurants CRUD</title>
@@ -31,11 +40,23 @@ $restaurants = $statement->fetchAll(PDO::FETCH_ASSOC);
     <h1>Restaurants CRUD</h1>
     <p>
       <a href="create.php" class="btn btn-success"> Créer un Restaurant </a>
+
+        <a href="login.php" class="btn btn-secondary"> Retour </a>
+
     </p>
+    <form>
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Recherche de restaurant" name="search" value="<?php echo $search;?>">
+        <button class="btn btn-outline-secondary" type="submit">Rechercher</button>
+      </div>
+    </form>
+
+
     <table class="table">
     <thead>
       <tr>
         <th scope="col"> Nombre </th>
+        <th scope="col">Nom</th>
         <th scope="col">Type</th>
         <th scope="col">Prix</th>
         <th scope="col">Adresse</th>
@@ -49,15 +70,20 @@ $restaurants = $statement->fetchAll(PDO::FETCH_ASSOC);
 
         <tr>
           <th scope="row"><?php echo $i + 1 ?></th>
-          
+          <td><?php echo $restaurant['nom'] ?></td>
           <td><?php echo $restaurant['type'] ?></td>
           <td><?php echo $restaurant['price'] ?></td>
           <td><?php echo $restaurant['adress'] ?></td>
           <td><?php echo $restaurant['note'] ?></td>
-          <td><?php echo $restaurant['image'] ?></td>
+          <td><a href="<?php echo $restaurant['image']?>"><img src="<?php echo $restaurant['image']?>"  class="thumb-image"> </a><td>
+
           <td>
-            <button type="button" class="btn btn-outline-primary"> Editer</button>
-            <button type="button" class="btn btn-outline-danger"> Supprimer</button>
+              <a href="update.php?id=<?php echo $restaurant['id']?>" class="btn btn-outline-primary"> Editer</a>
+
+           <form style="display: inline-block" method="post" action="delete.php">
+              <input type="hidden" name="id" value="<?php echo $restaurant['id']?>">
+              <button type="submit" class="btn btn-outline-danger"> Supprimer</button>
+            </form>
           </td>
         </tr>
       <?php endforeach;?>
