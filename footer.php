c896c2f611fab09d37ee9f788908e4d3a954a7ef
TitouanMonier <titouanmonier.pro@gmail.com>
2022-08-28 10:23:22 +0200

    ajout du navbar et du footer

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
-
-<div id='footer'>
-  <div id="footer-texts">
-    <div>© 2022 Restaurinaux, Inc.</div>
-    <div>Tous droits réservés. Conçu par Titouan MONIER.</div>
+<div id="footer-container">
+  <div id='footer'>
+    <div id="footer-texts">
+      <div>© 2022 Restaurinaux, Inc.</div>
+      <div>Tous droits réservés. Conçu par Titouan MONIER.</div>
+    </div>
   </div>
 </div>
--- navbar.php
+++ navbar.php
@@ -1,40 +1,41 @@
+<div id="navbar">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a href="."class="navbar-brand mb-0 h1">
+      <img class="d-inline-block "src="Logo.png" width="125"/>
+      Restaurinaux
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a href="."class="navbar-brand mb-0 h1">
-    <img class="d-inline-block "src="Logo.png" width="125"/>
-    Restaurinaux
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php?type=traditionnel">Traditionnel <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php?type=bistrot">Bistrot <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php?type=brasserie">Brasserie <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php?type=gastronomique">Gastronomique<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php?type=cuisine-du-monde">Cuisine du Monde<span class="sr-only">(current)</span></a>
-      </li>
-
+      <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="login.php">Se connecter<span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="/index.php?type=traditionnel">Traditionnel <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item active">
-          <a class="nav-link" href="login.php">Se déconnecter<span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="/index.php?type=bistrot">Bistrot <span class="sr-only">(current)</span></a>
         </li>
-      <div id= "filtres">
-        <div><h3> <?php echo 'Bonjour ' .$_SESSION['last_name']. '! :)'; ?></h3></div>
-      </div>
-    </ul>
-  </div>
-</nav>
+        <li class="nav-item active">
+          <a class="nav-link" href="/index.php?type=brasserie">Brasserie <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/index.php?type=gastronomique">Gastronomique<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/index.php?type=cuisine-du-monde">Cuisine du Monde<span class="sr-only">(current)</span></a>
+        </li>
+
+          <li class="nav-item active">
+            <a class="nav-link" href="/login.php">Se connecter<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/login.php">Se déconnecter<span class="sr-only">(current)</span></a>
+          </li>
+        <div id= "filtres">
+          <div><h3> <?php echo 'Bonjour ' .$_SESSION['last_name']. '! :)'; ?></h3></div>
+        </div>
+      </ul>
+    </div>
+  </nav>
+</div>
